159ca8a4e9d0b7778f63797d2e670b75f4bbcbd3
mrjadaml <mrjadaml@gmail.com>
2014-10-09 15:22:40 -0600

    converted to haml and added renders for home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,22 @@
 class PagesController < ApplicationController
+
+  def index
+    render 'home'
+  end
+
   def about
   end
 
   def terms
   end
+
+  def columns
+    @columns = [
+      Tasks, Documents, Comments
+    ]
+  end
+
 end
+
+
+# {title: Tasks, item: Grouped by projects and lists. Just the way you like em.}, {title: Documents, item: }
