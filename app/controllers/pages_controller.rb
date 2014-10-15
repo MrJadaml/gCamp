be31c152f93ad6d584715f9104b7a36b7d87984e
mrjadaml <mrjadaml@gmail.com>
2014-10-15 14:17:50 -0600

    put quotes into model and added a faq page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,6 +19,20 @@ class PagesController < ApplicationController
       }
     ]
 
+    quote1 = Quote.new
+    quote1.author = "-Atom"
+    quote1.quote = '"You da bomb"'
+
+    quote2 = Quote.new
+    quote2.author = "-Orca"
+    quote2.quote = '"This site looks killer"'
+
+    quote3 = Quote.new
+    quote3.author = "-Snoop"
+    quote3.quote = '"What a dope page"'
+
+    @quotes = [quote1, quote2, quote3]
+
     render 'home'
   end
 
