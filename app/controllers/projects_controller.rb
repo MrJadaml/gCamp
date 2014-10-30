090b16a5a7eacf4bddfdd255c386c2b969b221e1
mrjadaml <mrjadaml@gmail.com>
2014-10-30 14:00:05 -0600

    modified project views for show edit and index pages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,28 +9,41 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.new(project_parms)
+    @project = Project.new(project_params)
     @project.save
     redirect_to @project, notice: 'Created new project'
   end
 
   def show
-    @project = Project.find(params[:id])
+    set_project
   end
 
   def edit
+    set_project
   end
 
   def update
+    set_project
+    if @project.update(project_params)
+      redirect_to @project, notice: 'Project was updated'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    Project.find(params[:id]).destroy
+    redirect_to projects_path, notice: 'Project was destroyed!'
   end
 
   private
 
-  def project_parms
+  def project_params
     params.require(:project).permit(:name)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 
 end
