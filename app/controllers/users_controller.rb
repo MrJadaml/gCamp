25cb46dfae4fcf0d1a39b5555134350dd8ba879a
mrjadaml <mrjadaml@gmail.com>
2014-11-04 15:41:12 -0700

    cleaned up views and fixed controller issues related to the missing names regression

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to @user
+    if @user.save
+      redirect_to @user, notice: 'Successfully created user'
+    else
+      render :new
+    end
   end
 
   def show
