6369c92fc2150db63e1d8c8a4bd4230294b7c28c
mrjadaml <mrjadaml@gmail.com>
2014-11-23 11:44:58 -0700

    presence and uniqueness of users for memberships validated. Also fixed bug where role was not set on dropdown after creation

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,6 @@
 class Membership<ActiveRecord::Base
   belongs_to :user
   belongs_to :project
-  validates :user, :role, presence: :true, uniqueness: :true
+  validates :user, uniqueness: :true
+  validates :user, presence: :true
 end
